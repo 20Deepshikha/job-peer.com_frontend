9b5d24df03f625f0326c917685c6bf67ed9b7376
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-02-12 00:15:53 -0500

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 //setting the base url for ease
 const api = axios.create({
     // baseURL: 'http://localhost:8000/jat/'
-    baseURL: 'https://job-peer.onrender.com/'
+    baseURL: 'https://job-peer.onrender.com/jat'
 });
 
 export default api;
