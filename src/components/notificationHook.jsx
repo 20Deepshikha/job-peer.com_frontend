84754079e63fd6697e95a42e5f87510046e9de42
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-02-11 22:40:02 -0600

    trying different cors

--- src/components/notificationHook.jsx
+++ src/components/notificationHook.jsx
@@ -37,7 +37,8 @@ import io from 'socket.io-client';
 import api from '../config/axios';
 
 // const socket = io('http://localhost:8000', { withCredentials: true });
-const socket = io('https://job-peer.onrender.com', { withCredentials: true });
+// const socket = io('https://job-peer.onrender.com', { withCredentials: true });
+const socket = io('job-peer.onrender.com:10000', { withCredentials: true });
 
 const useNotifications = (username) => {
   const [notifications, setNotifications] = useState([]);
